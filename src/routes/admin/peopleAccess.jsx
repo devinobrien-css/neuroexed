e149f18108912b7626625ffdf97c8466ea391fb3
Devin O'Brien <61803565+devinobrien-css@users.noreply.github.com>
2023-08-28 08:24:56 -0400

    fixes

--- src/routes/admin/peopleAccess.jsx
+++ src/routes/admin/peopleAccess.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import { member, sort_order } from '../../shared/types/object_schema';
 import { fetchData,putData,removeData, uploadFileToBucket } from '../../shared/services/dba';
